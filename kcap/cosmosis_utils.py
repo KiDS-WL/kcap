cdfeef216db52c1547ac3a4a0d8b077cdf0a82f6
Tilman Troester <tilman.troester@gmail.com>
2018-12-17 17:38:33 +0000

    First attempt at KCAP module.

---
+++ kcap/cosmosis_utils.py
@@ -0,0 +1,254 @@
+import glob
+import collections
+import os
+import configparser
+import io
+
+import numpy as np
+
+import cosmosis.runtime.config
+import cosmosis.runtime.pipeline
+
+def config_to_cosmosis_ini(config):
+    ini = cosmosis.runtime.config.Inifile(None, )
+    with io.StringIO() as s:
+        config.write(s)
+        s.seek(0)
+        ini.read_file(s)
+    return ini
+
+def config_to_string(config):
+    with io.StringIO() as s:
+        config.write(s)
+        s.seek(0)
+        return s.read()
+
+class CosmoSISModule:
+    module_name = None
+    def __init__(self, module_file, base_path=None):
+        self.module_file = module_file
+        if base_path:
+            self.module_file =  os.path.join(base_path, module_file)
+
+        self.config = collections.OrderedDict()
+        self.config["file"] = self.module_file
+
+        self.default_parameters = {}
+
+    def get_section(self):
+        return {self.module_name : self.config}
+
+class ConsistencyModule(CosmoSISModule):
+    module_name = "consistency"
+    def __init__(self, module_file="utility/consistency/consistency_interface.py", base_path=r"%(CSL_PATH)s"):
+        super().__init__(module_file, base_path)
+
+class CAMBModule(CosmoSISModule):
+    module_name = "camb"
+    def __init__(self, module_file="boltzmann/camb/camb.so", base_path=r"%(CSL_PATH)s",
+                       z_min=0.0, z_max=6.0, n_z=100, transfer_function=True):
+        super().__init__(module_file, base_path)
+
+        self.config.update({"mode"              : "all" if transfer_function else "background",
+                            "lmax"              : "2500",
+                            "feedback"          : "0",
+                            "zmax"              : str(float(z_max)),
+                            "nz"                : str(n_z),
+                            "background_zmax"   : str(float(z_max)),
+                            "background_nz"     : str(int(z_max*100)),})
+
+        self.default_parameters = {"cosmological_parameters" : {"omega_m"  : "0.3",
+                                                                "h0"       : "0.70",
+                                                                "omega_b"  : "0.05",
+                                                                "tau"      : "0.089",
+                                                                "n_s"      : "0.96",
+                                                                "omega_k"  : "0.0",
+                                                                "w"       : "-1.0",
+                                                                "wa"      : "0.0",
+                                                                "A_s"      : "2.1e-9",}}
+
+class HMxModule(CosmoSISModule):
+    module_name = "HMx"
+    def __init__(self, module_file=r"lib/cosmosis_interface.so", base_path=r"%(HMX_PATH)s",
+                       transfer_function="camb",
+                       k_min=1e-3, k_max=1e1, n_k=128, 
+                       z_min=0.0, z_max=5.999, n_z=32,
+                       fields=["dmonly"],
+                       matter_matter_section_name="matter_power_nl",
+                       verbose=0, hm_mode="hmcode", one_parameter_hmcode=False,
+                       ihm=None):
+        super().__init__(module_file, base_path)
+
+        self.config.update({"fields"            : " ".join(fields),
+                            "nk"                : str(n_k),
+                            "kmin"              : str(float(k_min)),
+                            "kmax"              : str(float(k_max)),
+                            "nz"                : str(n_z),
+                            "zmin"              : str(float(z_min)),
+                            "zmax"              : str(float(z_max)),
+                            "verbose"           : str(verbose),
+                            "dimensionless_power_spectrum" : 0,
+                            "p_lin_source"      : "external" if transfer_function.lower() == "camb" else "eh",
+                            "matter_matter_section_name" : matter_matter_section_name,
+                            "hm_mode"           : hm_mode,
+                            "one_parameter_hmcode" : "T" if one_parameter_hmcode else "F"})
+        if ihm is not None:
+            self.config["ihm"] = str(ihm)
+
+        self.default_parameters = {"halo_model_parameters" : {"log10_Theat" : "7.8"}}
+        
+class LoadNofzModule(CosmoSISModule):
+    module_name = "load_nofz"
+    
+    def __init__(self, nofz_file, output_section="shear",
+                       module_file=r"number_density/load_nz/load_nz.py", base_path=r"%(CSL_PATH)s",):
+        super().__init__(module_file, base_path)
+        
+        if isinstance(nofz_file, str):
+            filepath = nofz_file
+        elif isinstance(nofz_file, (list, tuple)):
+            filepath = " ".join(nofz_file)
+        else:
+            raise ValueError("nofz_file needs to be str or list of str.")
+        self.config.update({"filepath"          : filepath,
+                            "histogram"         : "True",
+                            "output_section"    : f"nz_{output_section}",})
+
+class ProjectionModule(CosmoSISModule):
+    module_name = "projection"
+    
+    def __init__(self, probes=[], n_ell=400, ell_min=0.1, ell_max=5e4, verbose=True,
+                       module_file=r"structure/projection/project_2d.py", base_path=r"%(CSL_PATH)s"):
+        super().__init__(module_file, base_path)
+     
+        self.config.update({"ell_min"           : str(float(ell_min)),
+                            "ell_max"           : str(float(ell_max)),
+                            "n_ell"             : str(n_ell),
+                            "verbose"           : "T" if verbose else "F",
+                            "get_kernel_peaks"  : "F"})
+        self.config.update({"-".join(probe) : "-".join(probe) for probe in probes})
+
+
+class CosmoSISPipeline:
+    def __init__(self, paths={}, sampler="", parameter_file="", prior_file="", 
+                       verbose=0, debug=False):
+        self.paths = paths
+        self.paths["KCAP_PATH"] = os.path.split(os.path.dirname(__file__))[0]
+        if "CSL_PATH" not in paths:
+            self.paths["CSL_PATH"] = os.path.join(self.paths["KCAP_PATH"], "cosmosis-standard-library")
+
+        self.config_defaults = {key : path for key, path in self.paths.items()}
+
+        self.cosmosis_config = self.setup_pipeline_config(sampler="",
+                                   parameter_file=parameter_file, prior_file=prior_file, 
+                                   verbose=verbose, debug=debug)
+
+        self.modules = []
+
+    def create_pipeline(self, parameters=None):
+        ini = configparser.ConfigParser(defaults=self.config_defaults)
+        ini.read_dict(self.cosmosis_config)
+
+        ini["pipeline"]["modules"] = " ".join(m.module_name for m in self.modules)
+        for m in self.modules:
+            ini[m.module_name] = m.config
+
+        if ini["pipeline"]["values"] == "":
+            values = configparser.ConfigParser()
+            for m in self.modules:
+                values.read_dict(m.default_parameters)
+            if parameters is not None:
+                values.read_dict(parameters)
+            values = config_to_cosmosis_ini(values)
+        else:
+            values = None
+        
+        # print("INI", config_to_string(ini))
+        cosmosis_ini = config_to_cosmosis_ini(ini)
+        pipeline = cosmosis.runtime.pipeline.LikelihoodPipeline(cosmosis_ini, values=values)
+        return pipeline, ini
+
+    def run(self, parameters={}):
+        self.pipeline, self.pipeline_ini = self.create_pipeline()
+        p = [p.start for p in self.pipeline.parameters]
+        for section in parameters.keys():
+            for name, value in parameters[section].items():
+                idx = self.pipeline.parameter_index(section.lower(), name.lower())
+                p[idx] = value
+        data = self.pipeline.run_parameters(p, all_params=True)
+        return data
+
+    # def setup_base_config(self, output_path=None,
+    #                       sampler="", sampler_output_file=None, 
+    #                       project_triad_path=None, **sampler_kwargs):
+    #     cosmosis_config = configparser.ConfigParser()  
+
+    #     cosmosis_config["runtime"] =           {"sampler"               : sampler}
+
+    #     # if sampler_output_file is None:
+    #     #     sampler_output_file = os.path.join(output_path, "sampler_output.txt")
+
+    #     # cosmosis_config["output"] =            {"format"                : "text",
+    #     #                                         "filename"              : sampler_output_file}
+    #     # if sampler.lower() == "test":
+    #     #     cosmosis_config["test"] =          {"save_dir"               : r"%(OUTPUT_PATH)s",
+    #     #                                         "fatal_errors"           : "T"}
+    #     # if sampler.lower() == "fisher":
+    #     #     cosmosis_config["fisher"] = sampler_kwargs
+
+    #     return cosmosis_config
+
+    def setup_pipeline_config(self, sampler="",
+                                    parameter_file="", prior_file="", 
+                                    verbose=0, debug=False):
+        cosmosis_config = configparser.ConfigParser()  
+
+        cosmosis_config["runtime"] =     {"sampler"           : sampler}
+
+        cosmosis_config["pipeline"] =    {"values"            : parameter_file,
+                                          "priors"            : prior_file,
+                                          "likelihoods"       : "",
+                                          "extra_output"      : "",
+                                          "quiet"             : "T" if verbose == 0 else "F",
+                                          "timing"            : "T" if verbose > 1 else "F",
+                                          "debug"             : "T" if debug else "F"}
+        return cosmosis_config
+
+
+def load_cosmosis_params(output_path, section="cosmological_parameters"):
+    with open(os.path.join(output_path, section, "values.txt"), "r") as f:
+        params = {line.split("=")[0].strip() : float(line.split("=")[1].strip()) for line in f.readlines()}
+    return params
+
+def load_cosmosis_2pt(output_path, x_name="ell", y_name="shear", suffix="cl"):
+    cosmosis_2pt = {}
+
+    name = y_name + "_" + suffix
+    for path in glob.glob(os.path.join(output_path, f"{name}*")):
+        section = os.path.split(path)[1]
+        cosmosis_2pt[section] = {}
+
+        values = load_cosmosis_params(os.path.split(path)[0], section=section)
+        cosmosis_2pt[section] = {**values}
+        
+        if os.path.isfile(os.path.join(path, x_name+".txt")):
+            x_file = True
+            x = np.loadtxt(os.path.join(path, x_name+".txt"))
+        else:
+            x_file = False
+            
+        for bin_path in glob.glob(os.path.join(path, "*_*_*")):
+            key = os.path.split(bin_path)[1]
+            key = key[:key.rfind(".txt")]
+            data = np.loadtxt(bin_path).T
+            if x_file:
+                y = data
+            else:
+                x = data[0]
+                y = data[1]
+            
+            cosmosis_2pt[section][key] = y
+            
+        cosmosis_2pt[section][x_name] = x
+        
+    return cosmosis_2pt
---
+++ kcap/twopoint.py
@@ -0,0 +1,280 @@
+import collections
+import os
+import configparser
+import io
+
+import numpy as np
+from .cosmosis_utils import CosmoSISPipeline, ConsistencyModule, CAMBModule, HMxModule, ProjectionModule, LoadNofzModule
+
+def dict_insert(d, idx, obj, after=False):
+    if after: idx += 1
+    d_list = list(d.items())
+    return collections.OrderedDict(d_list[:idx] + [obj] + d_list[idx:])
+
+class TwoPoint:
+    def __init__(self, probes={}, parameters={},
+                 transfer_function="camb", nonlinear_Pk="HMx", 
+                 tomographic_bin_names={}, nofz_files={},
+                 module_paths={}):
+
+        self.probes = probes
+
+        statistics = []
+        fields_3D = []
+        fields_2D = []
+        for statistic, fields in probes.items():
+            statistics.append(statistic.lower())
+            for field1, field2 in fields:
+                if statistic.lower() == "pk":
+                    fields_3D += [field1, field2]
+                elif statistic.lower() == "cl" or statistic.lower() == "xi":
+                    fields_2D += [field1, field2]
+                else:
+                    raise ValueError(f"Statistic {statistic} not supported.")
+
+        # Fill in requirements if not already included
+        if "xi" in statistics:
+            statistics += ["pk", "cl"]
+        if "cl" in statistics:
+            statistics += ["pk"]
+        if "shear" in fields_2D:
+            fields_3D += ["matter"]
+        if "galaxy" in fields_2D:
+            fields_3D += ["matter"]
+        if "y" in fields_2D:
+            fields_3D += ["pressure"]
+
+        self.statistics = list(collections.OrderedDict().fromkeys(statistics))
+        self.fields_3D = list(collections.OrderedDict().fromkeys(fields_3D))
+        self.fields_2D = list(collections.OrderedDict().fromkeys(fields_2D))
+
+        self.tomographic_bin_names = tomographic_bin_names
+        self.nofz_files = nofz_files
+
+        self.parameters = parameters
+
+        self.pipeline_has_run = False
+        paths = {"HMX_PATH" : "%(KCAP_PATH)s/HMx/"}
+        paths.update(module_paths)
+
+        self.pipeline = CosmoSISPipeline(paths=paths)
+
+        self.modules = collections.OrderedDict()
+
+        self.add_module(("consistency", ConsistencyModule()))
+
+        if "pk" in self.statistics:
+            self.add_module(("camb", CAMBModule(transfer_function=transfer_function.lower() == "camb")))
+            if nonlinear_Pk.lower() == "hmx":
+                self.add_module(("HMx", HMxModule(hm_mode="hmx", fields=self.fields_3D, transfer_function=transfer_function)))
+            elif nonlinear_Pk.lower() == "hmcode":
+                self.add_module(("HMx", HMxModule(hm_mode="hmcode", transfer_function=transfer_function)))
+            else:
+                raise ValueError(f"Nonlinear P(k) method {nonlinear_Pk} not supported.")
+
+        if "cl" in self.statistics:
+            self.add_module(("projection", ProjectionModule(probes=probes["cl"])))
+            for field in self.fields_2D:
+                if field in nofz_files:
+                    self.add_module((f"load_nofz_{field}", 
+                                     LoadNofzModule(nofz_file=nofz_files[field],
+                                                    output_section=field)), before="projection")
+
+    def add_module(self, module, before=None, after=None):
+        if before is None and after is None:
+            self.modules[module[0]] = module[1]
+        elif before is not None:
+            idx = list(self.modules.keys()).index(before)
+            self.modules = dict_insert(self.modules, idx, module)
+        elif after is not None:
+            idx = list(self.modules.keys()).index(after)
+            self.modules = dict_insert(self.modules, idx, module, after=True)
+
+    def run_pipeline(self, parameters={}):
+        self.pipeline.modules = list(self.modules.values())
+        result = self.pipeline.run(parameters)
+        self.pipeline_has_run = True
+        return result
+
+    def power_spectrum(self, probes=("matter", "matter"), k=None, z=None, kind="nonlinear"):
+        if "pk" not in self.statistics:
+            raise ValueError(f"TwoPoint not initialized for 3D power spectra.")
+        if probes[0] not in self.fields_3D or probes[1] not in self.fields_3D:
+            raise ValueError(f"TwoPoint not initialized for probes {probes}.")
+        if not self.pipeline_has_run:
+            self.data = self.run_pipeline(self.parameters)
+
+        section = f"{probes[0]}_{probes[1]}_power_spectrum"
+        if probes == ("matter", "matter") and not self.data.has_section(section):
+            if kind.lower() == "nonlinear" and self.data.has_section("matter_power_nl"):
+                section = "matter_power_nl"
+            elif kind.lower() == "linear" and self.data.has_section("matter_power_lin"):
+                section = "matter_power_lin"
+        
+        k_grid = self.data[section, "k_h"]
+        z_grid = self.data[section, "z"]
+        pk_grid = self.data[section, "p_k"]
+
+        return pk_grid, k_grid, z_grid
+
+    def angular_power_spectrum(self, probes, ell=None,
+                               return_bin_names=False):
+        if "cl" not in self.statistics:
+            raise ValueError(f"TwoPoint not initialized for angular power spectra.")
+        if probes[0] not in self.fields_2D or probes[1] not in self.fields_2D:
+            raise ValueError(f"TwoPoint not initialized for probes {probes}.")
+        if not self.pipeline_has_run:
+            self.data = self.run_pipeline(self.parameters)
+
+        section = f"{probes[0]}_{probes[1]}_cl"
+        if probes == ("shear", "shear") and not self.data.has_section(section):
+            section = "shear_cl"
+        cl = []
+        bin_names = []
+        for _, key in self.data.keys(section):
+            if key[:3].lower() == "bin":
+                bin_1, bin_2 = key.split("_")[1:3]
+                cl.append(self.data[section, key])
+                if probes[0] in self.tomographic_bin_names:
+                    bin_1 = self.tomographic_bin_names[probes[0]][int(bin_1)-1]
+                if probes[1] in self.tomographic_bin_names:
+                    bin_2 = self.tomographic_bin_names[probes[1]][int(bin_2)-1]
+                bin_names.append((bin_1, bin_2))
+
+        cl = np.array(cl)
+        ell = self.data[section, "ell"]
+        if return_bin_names:
+            return cl, ell, bin_names
+        return cl, ell
+
+# class Predict2point(cosmosis_utils.CosmoSISPipeline):
+#     def __init__(self, probes=[], statistics=[], transfer_function="camb",
+#                  csl_path=None, HMx_path=None, project_triad_path=None):
+#         self.kcap_path = os.path.join(os.path.split(os.path.dirname(__file__))[0])
+#         if csl_path is None:
+#             self.csl_path = os.path.join(self.kcap_path, "cosmosis-standard-library")
+#         else:
+#             self.csl_path = csl_path
+        
+#         if HMx_path is None:
+#             self.HMx_path = os.path.join(self.kcap_path, "HMx")
+#         else:
+#             self.HMx_path = HMx_path
+
+#         self.cosmosis_config = self.setup_base_config()
+#         self.setup_pipeline_config(self.cosmosis_config)
+#         # self.setup_camb_config(self.cosmosis_config)
+        
+#         fields = set([p.replace("y", "pressure").replace("shear", "matter")
+#                         for probe in probes 
+#                             for p in probe.split("-")])
+#         self.setup_HMx_config(self.cosmosis_config, fields=fields)
+        
+#         self.setup_extrapolate_power(self.cosmosis_config)
+#         self.setup_projection_config(self.cosmosis_config)
+#         self.setup_cl_filter_config(self.cosmosis_config)
+#         self.setup_cl_binning_config(self.cosmosis_config)
+
+#         # ini = 
+#         # pipeline =
+#         # Fast-slow split
+
+
+
+
+    
+
+#         cosmosis_config["consistency"] = {"file"              : r"%(CSL_PATH)s/utility/consistency/consistency_interface.py",}
+
+#     def setup_camb_config(self, cosmosis_config, n_z, z_max):
+#         cosmosis_config["pipeline"]["modules"] += "camb "
+#         cosmosis_config["camb"] =        {"file"              : r"%(CSL_PATH)s/boltzmann/camb/camb.so",
+#                                           "mode"              : "all" if self.transfer_function.lower() == "camb" else "background",
+#                                           "lmax"              : "2500",
+#                                           "feedback"          : "0",
+#                                           "zmax"              : str(float(z_max)),
+#                                           "nz"                : str(n_z),
+#                                           "background_zmax"   : str(float(z_max)),
+#                                           "background_nz"     : str(int(z_max*100)),}
+
+#     def setup_HMx_config(self, cosmosis_config, n_k, k_min, k_max, n_z, z_min, z_max,
+#                          fields=["dmonly"],
+#                          verbose=0, hm_mode="hmcode", one_parameter_hmcode=False,
+#                          ihm=None):
+#         cosmosis_config["pipeline"]["modules"] += "HMx "
+#         cosmosis_config["HMx"] =         {"file"              : r"%(HMX_PATH)s/lib/cosmosis_interface.so",
+#                                           "fields"            : " ".join(fields),
+#                                           "nk"                : str(n_k),
+#                                           "kmin"              : str(float(k_min)),
+#                                           "kmax"              : str(float(k_max)),
+#                                           "nz"                : str(n_z),
+#                                           "zmin"              : str(float(z_min)),
+#                                           "zmax"              : str(float(z_max)),
+#                                           "verbose"           : str(verbose),
+#                                           "dimensionless_power_spectrum" : 0,
+#                                           "p_lin_source"      : "external" if self.transfer_function.lower() == "camb" else "eh",
+#                                           "hm_mode"           : hm_mode,
+#                                           "one_parameter_hmcode" : "T" if one_parameter_hmcode else "F"}
+#         if ihm is not None:
+#             cosmosis_config["HMx"]["ihm"] = str(ihm)                                
+
+#     def setup_nofz_config(self, cosmosis_config, nofz_file, output_section="nz_shear"):
+#         if isinstance(nofz_file, str):
+#             filepath = nofz_file
+#         elif isinstance(nofz_file, (list, tuple)):
+#             filepath = " ".join(nofz_file)
+#         else:
+#             raise ValueError("nofz_file needs to be str or list of str.")
+#         cosmosis_config["pipeline"]["modules"] += "load_nz "
+#         cosmosis_config["load_nz"] =     {"file"              : r"%(CSL_PATH)s/number_density/load_nz/load_nz.py",
+#                                           "filepath"          : filepath,
+#                                           "histogram"         : "True",
+#                                           "output_section"    : output_section,}
+
+#     def setup_projection_config(self, cosmosis_config, probes,
+#                                 n_ell=400, ell_min=0.1, ell_max=5e4, verbose=True):
+#         need_tSZ = False
+#         for probe in probes:
+#             if "y" in probe:
+#                 need_tSZ = True
+#         if need_tSZ:
+#             module_file = r"%(PROJECT_TRIAD_PATH)s/projection_tSZ/project_2d_tSZ.py"
+#         else:
+#             module_file = r"%(CSL_PATH)s/cosmosis-standard-library/structure/projection/project_2d.py"
+#         cosmosis_config["pipeline"]["modules"] += "projection "
+#         cosmosis_config["projection"] =  {"file"              : module_file,
+#                                           "ell_min"           : str(float(ell_min)),
+#                                           "ell_max"           : str(float(ell_max)),
+#                                           "n_ell"             : str(n_ell),
+#                                           "verbose"           : "T" if verbose else "F",
+#                                           "get_kernel_peaks"  : "F"}
+#         for probe in probes:
+#             cosmosis_config["projection"][probe] = probe.replace("cmbkappa", "k")
+
+#     def setup_cl_filter_config(self, cosmosis_config, probes, y_filter_fwhm):
+#         cosmosis_config["pipeline"]["modules"] += "filter_Cls "
+#         sections = []
+#         powers = []
+#         for probe in probes:
+#             p1, p2 = probe.split("-")
+#             if p1 == "y" or p2 == "y":
+#                 sections.append(f"{probe.replace('-', '_')}_cl")
+#                 if p1 == "y" and p2 == "y":
+#                     powers.append("2")
+#                 else:
+#                     powers.append("1")
+
+#         cosmosis_config["filter_Cls"] =           {"file"              : r"%(PROJECT_TRIAD_PATH)s/tools/filter_cls.py",
+#                                                    "filter"            : "gaussian",
+#                                                    "fwhm"              : str(y_filter_fwhm),
+#                                                    "sections"          : " ".join(sections),
+#                                                    "powers"            : " ".join(powers),}
+
+#     def setup_cl_binning_config(self, cosmosis_config, n_bin, ell_min, ell_max, logspaced=True):
+#         cosmosis_config["pipeline"]["modules"] += "bin_Cls "
+#         cosmosis_config["bin_Cls"] =              {"file"              : r"%(PROJECT_TRIAD_PATH)s/tools/bin_cls.py",
+#                                                    "ell_min"           : str(ell_min),
+#                                                    "ell_max"           : str(ell_max),
+#                                                    "n_bin"             : str(n_bin),
+#                                                    "logspaced"         : "T" if logspaced else "F",}
+        
