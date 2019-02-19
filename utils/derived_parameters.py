7d1a58acaa03b69eb23c385bb87fe7c8fac7577f
Tilman Troester <tilman.troester@gmail.com>
2019-02-19 14:29:04 +0000

    Support for sampling S_star. Added alpha parameter for S8 and S_star.

--- utils/derived_parameters.py
+++ utils/derived_parameters.py
@@ -4,27 +4,37 @@ from cosmosis.datablock.cosmosis_py import errors
 def setup(options):
     try:
         parameter_names = options.get_string(option_section, "parameters")
-        parameter_names = [p.strip() for p in parameter_names.split(" ")]
+        parameter_names = [p.strip().lower() for p in parameter_names.split(" ")]
         config = parameter_names
     except errors.BlockNameNotFound:
         parameter_names = []
 
+    config = {}
+    config["parameters"] = parameter_names
     for p in parameter_names:
-        if p not in ["S8"]:
+        if p not in ["s8", "s_star"]:
             raise ValueError(f"Derived parameter {p} not supported.")
+        if p == "s8" or p == "s_star":
+            config["alpha"] = options.get_string(option_section, "alpha", 0.5)
 
-    config = parameter_names
     return config
 
 def execute(block, config):
     parameter_names = config
-    for parameter_name in parameter_names:
-        if parameter_name == "S8":
+    for parameter_name in config["parameters"]:
+        if parameter_name == "s8":
+            alpha = config["alpha"]
             Omega_m = block["cosmological_parameters", "omega_m"]
             sigma_8 = block["cosmological_parameters", "sigma_8"]
-            S8 = sigma_8*(Omega_m/0.3)**0.5
-
+            S8 = sigma_8*(Omega_m/0.3)**alpha
             block["cosmological_parameters", "S8"] = S8
+        
+        if parameter_name == "s_star":
+            alpha = config["alpha"]
+            Omega_m = block["cosmological_parameters", "omega_m"]
+            A_s = block["cosmological_parameters", "A_s"]
+            S_star = A_s/2e-9*(Omega_m/0.3)**alpha
+            block["cosmological_parameters", "S_star"] = S_star
 
     return 0
 
---
+++ utils/sample_S_star.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from cosmosis.datablock import option_section
+
+def setup(options):
+    alpha = options.get_string(option_section, "alpha", 0.5)
+    return alpha
+
+def execute(block, config):
+    alpha = config
+    S_star = block["cosmological_parameters", "S_star"]
+    Omega_m = block["cosmological_parameters", "omega_m"]
+    A_s = S_star*2e-9/(Omega_m/0.3)**alpha
+    block["cosmological_parameters", "A_s"] = A_s
+
+def clean(config):
+    pass
