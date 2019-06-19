00edeeaec444163ca12f4785943d5bc7fa089026
Tilman Troester <tilman.troester@gmail.com>
2019-06-19 15:32:14 +0100

    BOSS with Planck priors.

--- utils/mini_BOSS_like.py
+++ utils/mini_BOSS_like.py
@@ -10,32 +10,44 @@ from cosmosis.datablock.cosmosis_py import errors
 
 
 def setup(options):
-    data_file = options[option_section, "data_vector_file"]
-    data_vector = np.loadtxt(data_file)
+    data_file = options.get_string(option_section, "data_vector_file").split()
+    
+    data_vector = {}
+    for i, f in enumerate(data_file):
+        b = i + 1
+        data_vector[b] = np.loadtxt(f)
 
-    cov_file = options[option_section, "covariance_file"]
-    cov = np.loadtxt(cov_file)
+        n_wedge = (data_vector[b].shape[1]-1)//2
+        n_points_full = data_vector[b].shape[0]
+        data_vector[b] = np.concatenate([data_vector[b][:,2*j+1] for j in range(n_wedge)])
 
+
+    cov_file = options.get_string(option_section, "covariance_file").split()
+
+    cov = {}
+    for i, f in enumerate(cov_file):
+        b = i + 1
+        cov[b] = np.loadtxt(f)
+
+    if set(data_vector.keys()) != set(cov.keys()):
+        raise RuntimeError("Entries in data_file and covariance_file are not consistent.")
     try:
         points_range = options[option_section, "points_range"]
     except errors.BlockNameNotFound:
         points_range = [4, 32]
-    
-    n_wedge = (data_vector.shape[1]-1)//2
-    n_points_full = data_vector.shape[0]
-    data_vector = np.concatenate([data_vector[:,2*i+1] for i in range(n_wedge)])
-    
+        
     cut_points_idx = list(range(points_range[0])) + list(range(points_range[1], n_points_full))
 
     if len(cut_points_idx) > 0:
         cut_points_idx = np.array(cut_points_idx, dtype=int)
         cut_points_idx = np.concatenate([cut_points_idx + n_points_full*i for i in range(n_wedge)])
 
-        data_vector = np.delete(data_vector, cut_points_idx) 
-        cov = np.delete(cov, cut_points_idx, axis=0)
-        cov = np.delete(cov, cut_points_idx, axis=1)
+        for b in data_vector.keys():
+            data_vector[b] = np.delete(data_vector[b], cut_points_idx) 
+            cov[b] = np.delete(cov[b], cut_points_idx, axis=0)
+            cov[b] = np.delete(cov[b], cut_points_idx, axis=1)
     
-    inv_cov = np.linalg.inv(cov)
+    inv_cov = {b : np.linalg.inv(cov[b]) for b in cov.keys()}
     
     like_name = options.get_string(option_section, "like_name")
     keep_theory_vector = options.get_bool(option_section, "keep_theory_vector", False)
@@ -44,14 +56,17 @@ def setup(options):
 def execute(block, config):
     inv_cov, data_vector, cut_points_idx, like_name, keep_theory_vector = config
     
-    n_wedge = block["xi_wedges", "n_wedge"]
-    mu = block["xi_wedges", "vtheo_convolved"]
-    mu = np.concatenate([m for m in mu])
-    x = data_vector
-    d = x - mu
+    # n_wedge = block["xi_wedges", "n_wedge"]
+    # mu = block["xi_wedges", "vtheo_convolved"]
+    chi2 = 0
+    for b in data_vector.keys():
+        mu = block[f"xi_wedges", f"bin_{b}"]
+        mu = np.concatenate([m for m in mu])
+        x = data_vector[b]
+        d = x - mu
 
-    chi2 = np.einsum('i,ij,j', d, inv_cov, d)
-    chi2 = float(chi2)
+        chi2 += float(np.einsum('i,ij,j', d, inv_cov[b], d))
+    
     like = -0.5*chi2
 
     block[names.data_vector, like_name+"_CHI2"] = chi2
