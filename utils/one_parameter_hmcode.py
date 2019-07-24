2bdb069350219861f23a400bb7c03b1ce9f15c07
Tilman Troester <tilman.troester@gmail.com>
2019-07-24 12:42:00 +0200

    New fiducial KV450 setup

---
+++ utils/one_parameter_hmcode.py
@@ -0,0 +1,18 @@
+import numpy as np
+from cosmosis.datablock import option_section, names
+
+def setup(options):
+    a_0 = options.get_double(option_section, "a_0", default=0.98)
+    a_1 = options.get_double(option_section, "a_1", default=-0.12)
+    config = [a_0, a_1]
+    return config
+
+def execute(block, config):
+    a_0, a_1 = config
+    A = block[names.halo_model_parameters, "As"]
+    eta = a_0 + a_1*A
+    block[names.halo_model_parameters, "eta0"] = eta
+    return 0
+
+def cleanup(config):
+    pass
