f14346d6845ea6b2838833f54e1f1df1d2e03150
Tilman Troester <tilman.troester@gmail.com>
2019-09-02 12:01:14 +0100

    New S_star definition and mock runs.

--- utils/sample_S_star.py
+++ utils/sample_S_star.py
@@ -4,20 +4,20 @@ from cosmosis.datablock import option_section
 
 def setup(options):
     alpha = options.get_double(option_section, "alpha", default=0.5)
-    use_ln_As = options.get_bool(option_section, "use_ln_A_s", default=False)
+    use_ln_As = options.get_bool(option_section, "use_ln_A_s", default=True)
     return alpha, use_ln_As
 
 def execute(block, config):
     alpha, use_ln_As = config
     if not use_ln_As:
         S_star = block["cosmological_parameters", "S_star"]
-        Omega_m = block["cosmological_parameters", "omega_m"]
-        A_s = S_star*2e-9/(Omega_m/0.3)**alpha
+        omch2 = block["cosmological_parameters", "omch2"]
+        A_s = S_star*3e-9/(omch2/0.1)**alpha
     else:
-        S_ln_star = block["cosmological_parameters", "S_ln_star"]
-        Omega_m = block["cosmological_parameters", "omega_m"]
-        ln_A_s_1e10 = S_ln_star/(Omega_m/0.3)**alpha
-        A_s = np.exp(ln_A_s_1e10)*1e-10    
+        S_star = block["cosmological_parameters", "S_star"]
+        omch2 = block["cosmological_parameters", "omch2"]
+        ln_As = S_star*3.0/(omch2/0.1)**alpha
+        A_s = np.exp(ln_As)*1e-10    
     block["cosmological_parameters", "A_s"] = A_s
     return 0
 
