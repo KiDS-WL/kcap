f6740e231cbe0aa25e9f3181d73af1cd6e4f6d6a
Tilman Troester <tilman.troester@gmail.com>
2019-11-12 11:46:41 +0000

    Allow hardcoded alpha values.

--- utils/magnification_alphas.py
+++ utils/magnification_alphas.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 from cosmosis.datablock import option_section, names
+from cosmosis.datablock.cosmosis_py import errors
 
 def setup(options):
-    alphas = options[option_section, "alpha_binned"]
+    try:
+        alphas = options[option_section, "alpha_binned"]
+    except errors.BlockNameNotFound:
+        # Use hard coded values:
+        alphas = np.array([1.0, 2.0])
+        
     return alphas
 
 def execute(block, config):
