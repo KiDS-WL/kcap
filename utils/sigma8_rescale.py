ede5ccdf89660bc7380991ba46078ceb0d9afa7d
Marika Asgari <ma@roe.ac.uk>
2019-12-10 16:46:29 +0000

    changed it such that if transfer only is set it doesnt break

--- utils/sigma8_rescale.py
+++ utils/sigma8_rescale.py
@@ -6,8 +6,6 @@ cosmo = section_names.cosmological_parameters
 cmb = section_names.cmb_cl
 matter_powspec_lin = section_names.matter_power_lin
 
-# Use this if onlu doing transfer (no CMB)
-
 
 def setup(options):
     return 0
@@ -19,10 +17,6 @@ def execute(block, config):
     sigma8_input = block[cosmo, 'sigma8_input']
     sigma8_camb = block[cosmo, 'sigma_8']
 
-    A_s = block[cosmo, 'A_s']
-    P_k_lin = block[matter_powspec_lin, 'P_k']
-
-
     zmin = block[matter_powspec_lin, 'z'].min()
     if zmin != 0.0:
         raise ValueError(
@@ -32,15 +26,36 @@ def execute(block, config):
     r = (sigma8_input**2) / (sigma8_camb**2)
 
     # Rescale CMB Cl and matter power outputs
+    A_s = block[cosmo, 'A_s']
     A_s *= r
-    P_k_lin *= r
-
-    # Save back to block
     block[cosmo, 'A_s'] = A_s
-    block[matter_powspec_lin, 'P_k'] = P_k_lin
-
-
     block[cosmo, 'sigma_8'] = sigma8_input
+
+    #check if power spectra exists
+    if(block.has_value(cmb, 'TT')):
+        TT = block[cmb, 'TT']
+        TT *= r
+        block[cmb, 'TT'] = TT
+
+    if(block.has_value(cmb, 'EE')):
+        EE = block[cmb, 'EE']
+        EE *= r
+        block[cmb, 'EE'] = EE
+
+    if(block.has_value(cmb, 'BB')):
+        BB = block[cmb, 'BB']
+        BB *= r
+        block[cmb, 'BB'] = BB
+
+    if(block.has_value(cmb, 'TE')):
+        TE = block[cmb, 'TE']
+        TE *= r
+        block[cmb, 'TE'] = TE
+
+    if(block.has_value(cmb, 'P_k')):
+        P_k_lin = block[matter_powspec_lin, 'P_k']
+        P_k_lin *= r
+        block[matter_powspec_lin, 'P_k'] = P_k_lin
 
     # signal that everything went fine
     return 0
