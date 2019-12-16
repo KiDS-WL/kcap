a62157c61a4fb1d4925a8dae2ce74fab803a09ce
Marika Asgari <ma@roe.ac.uk>
2019-12-16 15:31:49 +0000

    added a warning for nonlinear power. Included rescaling of cmb-lensing and growth parameters.

--- utils/sigma8_rescale.py
+++ utils/sigma8_rescale.py
@@ -5,9 +5,13 @@ from cosmosis.datablock import option_section
 cosmo = section_names.cosmological_parameters
 cmb = section_names.cmb_cl
 matter_powspec_lin = section_names.matter_power_lin
+growth_params = section_names.growth_parameters
 
 
 def setup(options):
+    print(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!WARNIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
+    print(" sigma8_rescale only deals with linear power spectra. Nonlinear power is not changed! ")
+    print(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     return 0
 
 
@@ -25,13 +29,22 @@ def execute(block, config):
     # Calculate rescale factor
     r = (sigma8_input**2) / (sigma8_camb**2)
 
-    # Rescale CMB Cl and matter power outputs
+    # rescale parameters first
     A_s = block[cosmo, 'A_s']
     A_s *= r
     block[cosmo, 'A_s'] = A_s
     block[cosmo, 'sigma_8'] = sigma8_input
 
-    #check if power spectra exists
+    if(block.has_value(growth_params, "sigma_8")):
+        block[growth_params, "sigma_8"] = sigma8_input
+
+    if(block.has_value(growth_params, "fsigma_8")):
+        fsigma8 = block[growth_params, "fsigma_8"]
+        fsigma8 *= sigma8_input/sigma8_camb
+        block[growth_params, "fsigma_8"] = fsigma8
+
+    # Check if power spectra exists
+    # Then Rescale CMB Cl and matter power outputs
     if(block.has_value(cmb, 'TT')):
         TT = block[cmb, 'TT']
         TT *= r
@@ -51,6 +64,21 @@ def execute(block, config):
         TE = block[cmb, 'TE']
         TE *= r
         block[cmb, 'TE'] = TE
+
+    if(block.has_value(cmb, 'PP')):
+        PP = block[cmb, 'PP']
+        PP *= r
+        block[cmb, 'PP'] = PP
+
+    if(block.has_value(cmb, 'PT')):
+        PT = block[cmb, 'PT']
+        PT *= r
+        block[cmb, 'PT'] = PT
+
+    if(block.has_value(cmb, 'PE')):
+        PE = block[cmb, 'PE']
+        PE *= r
+        block[cmb, 'PE'] = PE
 
     if(block.has_value(cmb, 'P_k')):
         P_k_lin = block[matter_powspec_lin, 'P_k']
