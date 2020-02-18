8edc824d732ef7e3ab3354b6a85a3577c49d52a8
Linc <calin@roe.ac.uk>
2020-02-18 17:03:23 +0000

    Added m-corr to cov in the twopoint file in mock creating; added indexing for noisy mocks

--- runs/methodology/create_mocks.py
+++ runs/methodology/create_mocks.py
@@ -2,9 +2,16 @@ import subprocess
 
 if __name__ == "__main__":
 
+    if len(sys.argv) > 1:
+        noise_begin = int(sys.argv[1])
+        noise_end   = int(sys.argv[2])
+    else:
+        noise_begin = 0
+        noise_end   = 1
+
     script = "utils/run_kcap.py"
     
-    KiDS_twopoint_file =  "../K1000-data/Phase-1/twopoint/twoPoint_PneE+PeeE_mean_None_cov_theoryEgretta_nOfZ_bucerosBroad.fits"
+    KiDS_twopoint_file =  "../K1000-data/Phase-1/twopoint/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad.fits"
 
     run_name_root = "base"
     # Create 3x2pt data vector
@@ -12,21 +19,21 @@ if __name__ == "__main__":
 
     # Create noiseless mock data vector
     output_dir = "runs/methodology/data/noisefree_fiducial/"
-    run_name = f"{run_name_root}_{run_type}"
+    
+    if noise_begin == 0:
+        run_name = f"{run_name_root}_{run_type}"
 
-    cmd = ["--create-mocks", "--noiseless-mocks",
-            "--root-dir", output_dir,
-            "--KiDS-data-file", KiDS_twopoint_file,
-            "--run-name", run_name,
-            "--run-type", run_type]
-    subprocess.run(["python", script] + cmd, check=True)
+        cmd = ["--create-mocks", "--noiseless-mocks",
+                "--root-dir", output_dir,
+                "--KiDS-data-file", KiDS_twopoint_file,
+                "--run-name", run_name,
+                "--run-type", run_type]
+        subprocess.run(["python", script] + cmd, check=True)
 
     # Create noisy mocks
     output_dir = "runs/methodology/data/noisy_fiducial/"
 
-    n_noise_mocks = 1
-
-    for i in range(n_noise_mocks):
+    for i in range(noise_begin, noise_end):
         run_name = f"{run_name_root}_{i}_{run_type}"
 
         cmd = ["--create-mocks",
