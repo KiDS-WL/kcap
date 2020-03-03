bab00ae729bbd3a0a5c524ac28f5575b315acaf8
Linc <calin@roe.ac.uk>
2020-03-03 11:44:21 +0000

    Mocks & configs for MAP parameter testing

--- runs/methodology/create_test_MAP_parameters_mocks.py
+++ runs/methodology/create_test_MAP_parameters_mocks.py
@@ -1,55 +1,79 @@
-import subprocess
-import sys
 import argparse
-sys.path.append("modules/scale_cuts/")
-from wrapper_twopoint import TwoPointWrapper
 
-    
+
+def sample_random_starting_point():
+    priorDict = {
+        'cosmological_parameters': {
+            'omch2': [0.051, 0.2], 
+            'ombh2': [0.019, 0.026], 
+            'h0': [0.64, 0.82], 
+            'n_s': [0.84, 1.1], 
+            'S_8_input': [0.1, 1.3]
+        }, 
+
+        'halo_model_parameters': {
+            'A': [2.0, 3.13]
+        },
+
+        'intrinsic_alignment_parameters': {
+            'A': [-6.0, 6.0]
+        },
+
+        'nofz_shifts': {
+            'p_1': [-5.0, 5.0], 
+            'p_2': [-5.0, 5.0], 
+            'p_3': [-5.0, 5.0], 
+            'p_4': [-5.0, 5.0], 
+            'p_5': [-5.0, 5.0]
+        }, 
+
+        'bias_parameters': {
+            'b1_bin_1': [0.5, 9.0], 
+            'b2_bin_1': [-4.0, 8.0], 
+            'gamma3_bin_1': [-8.0, 8.0], 
+            'a_vir_bin_1': [0.0, 12.0], 
+            'b1_bin_2': [0.5, 9.0], 
+            'b2_bin_2': [-4.0, 8.0], 
+            'gamma3_bin_2': [-8.0, 8.0], 
+            'a_vir_bin_2': [0.0, 12.0]
+        }
+    }
+
+    starting_point_settings = []
+
+    for key1, value1 in priorDict:
+        for key2, value2 in value1:
+            p = np.random.uniform(value2[0], value2[1], 1)[0]
+            starting_point_settings.append("--set-parameters")
+            starting_point_settings.append(key1)
+            starting_point_settings.append(key2)
+            starting_point_settings.append(f'{value2[0]} {p} {value2[1]}')
+
+    return starting_point_settings
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--noise-free', action='store_true', help='create noise-free mocks')
+    #parser.add_argument('--noise-free', action='store_true', help='create noise-free mocks')
     parser.add_argument('--noise-range', nargs=2, default=[0, 1], type=int, metavar=('BEGIN', 'END'), help='create noisy mocks indexed by i with BEGIN <= i < END')
     args = parser.parse_args()
 
     script = "utils/run_kcap.py"
-    
-    data_name_root = "random_start"
 
-    # Always create 3x2pt data vector for mocks
-    # Configs know how to use only part of them properly
-    run_type = "EE_nE_w"
-    
-    KiDS_twopoint_file = "../K1000-data/Phase-1/twopoint/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad.fits"
-    
-    # Always use identity matrix to generate mocks
-    # Otherwise dz parameters will be transformed by the covariance
-    dz_cov_file = "data/KV450/nofz/id_cov.asc"
+    n_start_begin = 0
+    n_start_end   = 1
 
-    # Create noiseless mock data vector
-    if args.noise_free:
-        output_dir = "runs/methodology/data/noisefree_fiducial/"
-        data_name = f"{data_name_root}_{run_type}"
-        
-        cmd = ["--create-mocks", "--noiseless-mocks",
-                "--root-dir", output_dir,
-                "--KiDS-data-file", KiDS_twopoint_file,
-                "--dz-covariance-file", dz_cov_file,
-                "--run-name", data_name,
-                "--run-type", run_type]
-        subprocess.run(["python", script] + cmd, check=True)
+    for j in range(n_start_begin, n_start_end):
 
-    # Create noisy mocks
-    #output_dir = "runs/methodology/data/noisy_fiducial/"
+        # Do nothing for noise-free cases
+        #if args.noise_free:
+            #pass
 
-    #for i in range(args.noise_range[0], args.noise_range[1]):
-        #data_name = f"{data_name_root}_{i}_{run_type}"
+        # Create noisy mocks
+        output_dir = "runs/methodology/data/noisy_fiducial/random_start{j}/"
+        os.makedirs(f'{output_dir}', exist_ok=True)
 
-        #cmd = ["--create-mocks",
-                #"--root-dir", output_dir,
-                #"--KiDS-data-file", KiDS_twopoint_file,
-                #"--dz-covariance-file", dz_cov_file,
-                #"--run-name", data_name,
-                #"--run-type", run_type]
-        #subprocess.run(["python", script] + cmd, check=True)
+        for i in range(args.noise_range[0], args.noise_range[1]):
+            starting_point_settings = sample_random_starting_point()
+            np.save('{output_dir}start{j}_noise{i}.npy', starting_point_settings)
 
