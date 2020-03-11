44c87d9e4d59aedf5ba05e0c8dedf23fcd081bdd
Linc <calin@roe.ac.uk>
2020-03-11 17:51:01 +0000

    New sabotage mocks (there are two now)

--- runs/methodology/create_test_goodness_of_fit_mocks.py
+++ runs/methodology/create_test_goodness_of_fit_mocks.py
@@ -23,17 +23,18 @@ if __name__ == "__main__":
     # Always create 3x2pt data vector for mocks
     # Configs know how to use only part of them properly
     run_type = "EE_nE_w"
-    data_name_root = 'sabotaged'
+    data_name_root_2 = 'sabotaged2'
+    data_name_root_5 = 'sabotaged5'
     KiDS_twopoint_file = f"../K1000-data/Phase-1/twopoint/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad.fits"
-    dz_cov_file = "data/KV450/nofz/id_cov.asc"
+    dz_cov_file = "data/KV450/nofz/SOM_cov_multiplied_diagonal.asc"
 
     # Create noiseless mock data vector
     if args.noise_free:
         output_dir = "runs/methodology/data/noisefree_fiducial/"
-        data_name = f"{data_name_root}_{run_type}"
-        dz_sabotage_settings = [
-            "--set-parameters", "nofz_shifts", "p_3", "0.023",
-            "--set-parameters", "nofz_shifts", "p_4", "0.012"
+        data_name = f"{data_name_root_2}_{run_type}"
+        dz_sabotage_settings_2 = [
+            "--set-parameters", "nofz_shifts", "p_3", "2.0", ## This means 2-sigma.
+            "--set-parameters", "nofz_shifts", "p_4", "2.0"
         ]
 
         cmd = ["--create-mocks", "--noiseless-mocks",
@@ -42,7 +43,22 @@ if __name__ == "__main__":
                 "--dz-covariance-file", dz_cov_file,
                 "--run-name", data_name,
                 "--run-type", run_type,
-                *dz_sabotage_settings]
+                *dz_sabotage_settings_2]
+        subprocess.run(["python", script] + cmd, check=True)
+
+        data_name = f"{data_name_root_5}_{run_type}"
+        dz_sabotage_settings_5 = [
+            "--set-parameters", "nofz_shifts", "p_3", "5.0", ## This means 5-sigma.
+            "--set-parameters", "nofz_shifts", "p_4", "5.0"
+        ]
+
+        cmd = ["--create-mocks", "--noiseless-mocks",
+                "--root-dir", output_dir,
+                "--KiDS-data-file", KiDS_twopoint_file,
+                "--dz-covariance-file", dz_cov_file,
+                "--run-name", data_name,
+                "--run-type", run_type,
+                *dz_sabotage_settings_5]
         subprocess.run(["python", script] + cmd, check=True)
 
     # Create noisy mocks
@@ -51,7 +67,7 @@ if __name__ == "__main__":
     # and adding it to old mocks
     output_dir = "runs/methodology/data/noisy_fiducial/"
     KiDS_twopoint_file_base = f"runs/methodology/data/noisefree_fiducial/base_{run_type}/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noiseless.fits"
-    KiDS_twopoint_file_sabo = f"runs/methodology/data/noisefree_fiducial/{data_name_root}_{run_type}/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noiseless.fits" 
+    KiDS_twopoint_file_sabo = f"runs/methodology/data/noisefree_fiducial/{data_name_root_2}_{run_type}/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noiseless.fits" 
 
     TP_cov     = TwoPointWrapper.from_fits(KiDS_twopoint_file, covmat_name='COVMAT')
     mean_base  = TwoPointWrapper.from_fits(KiDS_twopoint_file_base, covmat_name='COVMAT').makeMeanVector()
@@ -59,7 +75,28 @@ if __name__ == "__main__":
     delta_mean = mean_sabo - mean_base
 
     for i in range(args.noise_range[0], args.noise_range[1]):
-        data_name = f"{data_name_root}_{i}_{run_type}"
+        data_name = f"{data_name_root_2}_{i}_{run_type}"
+
+        KiDS_twopoint_file_noisy = f"{output_dir}base_{i}_EE_nE_w/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noisy.fits"
+        KiDS_twopoint_file_save  = f"{output_dir}{data_name}/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noisy.fits"
+
+        os.makedirs(f'{output_dir}{data_name}/data/KiDS/', exist_ok=True)
+        TP_mean = TwoPointWrapper.from_fits(KiDS_twopoint_file_noisy)
+        mean    = TP_mean.makeMeanVector()
+        TP_cov.replaceMeanVector(mean + delta_mean)
+        TP_cov.to_fits(KiDS_twopoint_file_save, overwrite=True)
+
+    output_dir = "runs/methodology/data/noisy_fiducial/"
+    KiDS_twopoint_file_base = f"runs/methodology/data/noisefree_fiducial/base_{run_type}/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noiseless.fits"
+    KiDS_twopoint_file_sabo = f"runs/methodology/data/noisefree_fiducial/{data_name_root_5}_{run_type}/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noiseless.fits" 
+
+    TP_cov     = TwoPointWrapper.from_fits(KiDS_twopoint_file, covmat_name='COVMAT')
+    mean_base  = TwoPointWrapper.from_fits(KiDS_twopoint_file_base, covmat_name='COVMAT').makeMeanVector()
+    mean_sabo  = TwoPointWrapper.from_fits(KiDS_twopoint_file_sabo, covmat_name='COVMAT').makeMeanVector()
+    delta_mean = mean_sabo - mean_base
+
+    for i in range(args.noise_range[0], args.noise_range[1]):
+        data_name = f"{data_name_root_5}_{i}_{run_type}"
 
         KiDS_twopoint_file_noisy = f"{output_dir}base_{i}_EE_nE_w/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noisy.fits"
         KiDS_twopoint_file_save  = f"{output_dir}{data_name}/data/KiDS/twoPoint_PneE+PeeE_mean_None_cov_theoryEgrettaMCorr_nOfZ_bucerosBroad_mock_noisy.fits"
