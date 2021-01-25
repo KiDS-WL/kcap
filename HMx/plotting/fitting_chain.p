64110f1a1787df585fb18c6f93ff227444ac4c4d
Tilman Troester <tilman.troester@gmail.com>
2021-01-25 16:27:57 +0000

    Cleanup (#23)
    * Removed old examples folder.
    
    * Removed old HMx
    
    * More cleanup.

--- HMx/plotting/fitting_chain.p
+++
@@ -1,7 +0,0 @@
-reset
-
-chain='fitting/test_chain.dat'
-
-np=12
-
-plot for [i=1:np] chain u (column(0)):(column(i+1)) w p noti
--- HMx/setup_cosmosis_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# This shouldn't be necessary anymore soon.
-export COSMOSIS_SRC_DIR=`python -c "import cosmosis; import os; print(os.path.split(os.path.dirname(cosmosis.__file__))[0])"`
