ac2c6f9433c8ee0ef1bd538a6e1aed330996dfc7
Tilman Troester <tilman.troester@gmail.com>
2023-04-17 13:07:56 +0900

    More cleanup in prep for v2

--- cosebis/modules/calChiS.h
+++
@@ -1,8 +0,0 @@
-#ifndef CALCHIS_H
-#define CALCHIS_H
-
-#include "matrix.h"
-
-number calChiS(matrix theory,matrix data,matrix Cov);
-
-#endif
--- cosebis/modules/tolower.cc
+++
@@ -1,8 +0,0 @@
-#include <cstring>
-#include "tolower.h"
-
-void toLower(string& str){
-  int s=str.size(),i=0;
-  for (i=0;i<s;i++)
-    str[i]=tolower(str[i]);
-}
